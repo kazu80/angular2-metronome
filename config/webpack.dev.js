ea51603044aea38da472c30404a49b7178c47a5d
monkick <saruyama.monki@gmail.com>
2016-09-25 08:30:38 +0900

    Initial commit

---
+++ config/webpack.dev.js
@@ -0,0 +1,34 @@
+var webpackMerge = require ('webpack-merge');
+var ExtractTextPlugin = require ('extract-text-webpack-plugin');
+var commonConfig = require ('./webpack.common.js');
+var helpers = require ('./helpers');
+
+module.exports = webpackMerge (commonConfig, {
+    devtool: 'cheap-module-eval-source-map',
+
+    output: {
+        path         : helpers.root ('dist'),
+        publicPath   : 'http://localhost:8080/',
+        filename     : '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    plugins: [
+        new ExtractTextPlugin ('[name].css')
+    ],
+
+    devServer: {
+        historyApiFallback: true,
+        stats             : 'minimal'
+    },
+
+    module: {
+        loaders: [
+            {
+                test   : /\.scss$/,
+                exclude: /node_modules/,
+                loaders: ['raw-loader', 'sass-loader'] // sass-loader not scss-loader
+            }
+        ]
+    }
+});
